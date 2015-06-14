cf503ac8666062c14be3eddfcb2ba5cf07374188
Max Kaye <m@xk.io>
2015-06-14 19:33:09 +1000

    work on height not hash, hopefully will be faster

--- opreturnninja/rpc_scan.py
+++ opreturnninja/rpc_scan.py
@@ -20,11 +20,9 @@ if __name__ == "__main__":
     session = DBSession
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--block-hash', help='Start from this block hash', type=str, required=True)
+    parser.add_argument('--block-height', help='Start from this block height', type=int, required=True)
     args = parser.parse_args()
-
-    last_scanned_block_hash = None
-    next_block_hash = args.block_hash
+    block_height = args.block_height
 
     bitcoind = bitcoinrpc.connect_to_local().proxy
 
@@ -35,24 +33,21 @@ if __name__ == "__main__":
         return BytesIO(unhexlify(get_block(block_hash, False)))
 
     while True:
-        if next_block_hash == last_scanned_block_hash:  # this means we're waiting for the next block
-            json_block = get_block(next_block_hash)
-            if 'nextblockhash' in json_block:
-                next_block_hash = json_block['nextblockhash']
-                continue
-            sleep(1)
-            continue
-        else:
-            block = Block.parse(block_as_bytesio(next_block_hash))
+        try:
+            block_hash = bitcoind.getblockhash(block_height)
+            block = Block.parse(block_as_bytesio(block_hash))
             for tx_n, tx in enumerate(block.txs):
                 for tx_out_n, tx_out in enumerate(tx.txs_out):
                     if tx_out.script[0:1] == b'\x6a':
                         script_object = script_obj_from_script(tx_out.script)
                         if type(script_object) == ScriptNulldata:
-                            session.merge(Nulldatas(in_block_hash=last_scanned_block_hash, txid=hexlify(tx.hash()[::-1]), script=hexlify(tx_out.script), tx_n=tx_n, tx_out_n=tx_out_n))
-                            print(script_object, tx.hash(), next_block_hash)
+                            session.merge(Nulldatas(in_block_hash=block_hash, txid=hexlify(tx.hash()[::-1]), script=hexlify(tx_out.script), tx_n=tx_n, tx_out_n=tx_out_n))
+                            print(script_object, tx.hash(), block_height)
                             session.commit()
             session.commit()
 
-            last_scanned_block_hash = next_block_hash
-            print('Scanned', last_scanned_block_hash)
+            print('Scanned', block_height)
+            block_height += 1
+        except:
+            session.rollback()
+            sleep(1)
