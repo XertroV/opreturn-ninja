87b051a4d97f54af16c37c118be654243c8b36cd
Max Kaye <m@xk.io>
2016-10-07 12:01:30 +1100

    Add a print statement; heroku not building :/

--- application.py
+++ application.py
@@ -4,5 +4,6 @@ from waitress import serve
 from opreturnninja.config import config
 
 if __name__ == "__main__":
+    print("Loading application.py")
     app = loadapp('config:production.ini', relative_to='.')
     serve(app, host='0.0.0.0', port=config.PORT)
