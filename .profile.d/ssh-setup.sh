1abc71d68425d0a34f2e4648cf22a83fff3f72bd
Max Kaye <m@xk.io>
2016-10-07 12:58:03 +1100

    remove options

--- .profile.d/ssh-setup.sh
+++ .profile.d/ssh-setup.sh
@@ -17,7 +17,7 @@ chmod 600 ${HOME}/.ssh/heroku_id_rsa
 echo "bitcoin.xk.io ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBPQkI+B0IjWSEydHriSxN/LjwB9MP3YTYNneyJK2zTy9n1z5kmdGhxkhKePSSXUFMOIRhL7gxRbH4hgE33b58I0=" > ${HOME}/.ssh/known_hosts
 
 # Start the SSH tunnel if not already running
-SSH_CMD="ssh -f -v -o 'ServerAliveInterval 30' -o 'ServerAliveCountMax 3' -i ${HOME}/.ssh/heroku_id_rsa -N -L 8332:127.0.0.1:8332 ${BITCOIN_SSH_USER}@${BITCOIN_HOST}"
+SSH_CMD="ssh -f -v -i ${HOME}/.ssh/heroku_id_rsa -N -L 8332:127.0.0.1:8332 ${BITCOIN_SSH_USER}@${BITCOIN_HOST}"
 echo "SSH_CMD: ${SSH_CMD}"
 
 PID=`pgrep -f "${SSH_CMD}"`
