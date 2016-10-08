bc2f5813e480db2e1a0bbd4547974fcc23a5a5d4
Max Kaye <m@xk.io>
2016-10-08 15:33:15 +1100

    Up timeout

--- opreturnninja/compatibility.py
+++ opreturnninja/compatibility.py
@@ -4,7 +4,7 @@ from bitcoinrpc.authproxy import AuthServiceProxy
 
 from .config import config
 
-def gen_bitcoind(timeout=1):
+def gen_bitcoind(timeout=2):
     return AuthServiceProxy("http://%s:%s@%s:%d"%(config.BITCOIN_RPC_USER, config.BITCOIN_RPC_PASSWORD, config.BITCOIN_RPC_HOST, config.BITCOIN_RPC_PORT), timeout=timeout)
 
 bitcoind = gen_bitcoind()
