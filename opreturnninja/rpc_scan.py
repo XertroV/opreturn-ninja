a1d438d5e11fdcd02d6b5c67478070bd0788f775
Max Kaye <m@xk.io>
2016-10-11 10:41:12 +1100

    remove queue and refactor rpc_scan a bit

--- opreturnninja/rpc_scan.py
+++ opreturnninja/rpc_scan.py
@@ -21,11 +21,36 @@ from .models import DBSession, merge_nulldatas_from_block_obj, have_block, max_b
 from .compatibility import gen_bitcoind
 
 
-def f(func, args):
-    return func(*args)
+def get_block(bitcoind, block_hash, hex_summary=True):
+    return bitcoind.getblock(block_hash, hex_summary)
 
-def fstar(args):
-    return f(*args)
+
+def block_as_bytesio(bitcoind, block_hash):
+    return BytesIO(unhexlify(get_block(bitcoind, block_hash, False)))
+
+
+def block_at_height(block_height):
+    _bitcoind = gen_bitcoind(timeout=5)
+
+    while True:
+        try:
+            block_hash = _bitcoind.getblockhash(block_height)
+            block = Block.parse(block_as_bytesio(_bitcoind, block_hash))
+            ans = (block, block_hash, block_height)  # order important for merge_nulldatas
+            logging.info("Processing results for height %d" % ans[2])
+            merge_nulldatas_from_block_obj(*ans, bitcoind=_bitcoind)
+            return ans
+        except timeout as e:
+            logging.warning('%d, Timeout... Creating new bitcoind' % block_height)
+        except http.client.CannotSendRequest as e:
+            logging.warning("Got CannotSendRequest.")
+            traceback.print_tb(e.__traceback__)
+        except Exception as e:
+            logging.warning("%d, %s, %s" % (block_height, e, type(e)))
+        finally:
+            _bitcoind = gen_bitcoind()
+            logging.info("%d; Regen'd bitcoind" % block_height)
+            sleep(0.1)
 
 
 if __name__ == "__main__":
@@ -35,71 +60,44 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--block-height', help='Start from this block height', type=int, required=True)
     parser.add_argument('--n-processes', help='How many processes to use, can be higher than CPU#', type=int, default=1)
+    parser.add_argument('--cprofile', help='Use cProfile to monitor performance', action="store_true")
     args = parser.parse_args()
     init_bh = args.block_height
     n_proc = args.n_processes
     logging.info("Got args: %s" % args)
 
-    pace_q_size = n_proc
-    pace_q = mp.Queue(pace_q_size)
-    for i in range(pace_q_size - 2):
-        pace_q.put(True)
+    def main():
+        bitcoind = gen_bitcoind(timeout=15)
 
-    def get_block(bitcoind, block_hash, hex_summary=True):
-        return bitcoind.getblock(block_hash, hex_summary)
+        try:
+            best_block = bitcoind.getbestblockhash()
+            force_from = bitcoind.getblock(best_block)['height'] - 144  # force rescan of last day at least
+        except Exception as e:
+            logging.error("Got exception during startup: %s" % e)
+            sys.exit(1)
 
-    def block_as_bytesio(bitcoind, block_hash):
-        return BytesIO(unhexlify(get_block(bitcoind, block_hash, False)))
+        # TODO: figure out how to ensure we always have the correct nulldatas available in case of reorg
+        logging.info("Top block hash: %s" % best_block)
 
-    def block_at_height(block_height):
-        _bitcoind = gen_bitcoind(timeout=5)
-        pace_q.get()
+        start_scan_from = min(force_from, max_block_height(), init_bh)
+        logging.info("Scanning from %s" % start_scan_from)
 
-        while True:
-            try:
-                block_hash = _bitcoind.getblockhash(block_height)
-                block = Block.parse(block_as_bytesio(_bitcoind, block_hash))
-                ans = (block, block_hash, block_height)  # order important for merge_nulldatas
-                logging.info("Processing results for height %d" % ans[2])
-                merge_nulldatas_from_block_obj(*ans, bitcoind=_bitcoind)
-                return ans
-            except timeout as e:
-                logging.warning('%d, Timeout... Creating new bitcoind' % block_height)
-            except http.client.CannotSendRequest as e:
-                logging.warning("Got CannotSendRequest.")
-                traceback.print_tb(e.__traceback__)
-            except Exception as e:
-                logging.warning("%d, %s, %s" % (block_height, e, type(e)))
-            finally:
-                _bitcoind = gen_bitcoind()
-                logging.info("%d; Regen'd bitcoind" % block_height)
-                sleep(0.1)
+        _ah = all_block_heights()
+        logging.info("Reporting %d blocks scanned" % len(_ah))
+        existing_heights = set(_ah)
 
-    bitcoind = gen_bitcoind(timeout=15)
+        args = [i for i in range(start_scan_from + 1, force_from + (144 * 365)) if i not in existing_heights]
+        pool = mp.Pool(n_proc)
+        results = pool.imap(block_at_height, args)
+        logging.info("Got results object %s" % results)
 
-    try:
-        best_block = bitcoind.getbestblockhash()
-        force_from = bitcoind.getblock(best_block)['height'] - 144  # force rescan of last day at least
-    except Exception as e:
-        logging.error("Got exception during startup: %s" % e)
-        sys.exit(1)
+        for r in results:
+            logging.info("Finished processing %s, %s" % (r[1], r[2]))
+        pool.join()
 
-    # TODO: figure out how to ensure we always have the correct nulldatas available in case of reorg
-    logging.info("Top block hash: %s" % best_block)
 
-    start_scan_from = min(force_from, max_block_height(), init_bh)
-    logging.info("Scanning from %s" % start_scan_from)
-
-    _ah = all_block_heights()
-    logging.info("Reporting %d blocks scanned" % len(_ah))
-    existing_heights = set(_ah)
-
-    args = [i for i in range(start_scan_from + 1, force_from + (144 * 365)) if i not in existing_heights]
-    pool = mp.Pool(n_proc)
-    results = pool.imap(block_at_height, args)
-    logging.info("Got results object %s" % results)
-
-    for r in results:
-        logging.info("Finished processing %s, %s" % (r[1], r[2]))
-        pace_q.put(True)
-    pool.join()
+    if args.cprofile:
+        import cProfile
+        cProfile.run("main()")
+    else:
+        main()
